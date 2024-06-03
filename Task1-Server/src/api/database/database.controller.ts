e477c5b92b50ad02eee59eb4084e5a24cf6120c7
KrykukhaMaksymNure <maksym.krykukha@nure.ua>
2024-06-03 03:17:56 +0300

    add db module

---
+++ Task1-Server/src/api/database/database.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Post, Res, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { DatabaseService } from './database.service';
+import { AuthGuard } from '../auth/auth.guard';
+
+@Controller('api/database')
+export class DatabaseController {
+    constructor(private readonly databaseService: DatabaseService){}
+
+    @Get('export')
+    async exportToExcel(
+      @Res() res: Response,
+    ) {
+      const buffer = await this.databaseService.exportToExcel();
+      res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+      res.setHeader('Content-Disposition', 'attachment; filename=users.xlsx');
+      res.send(buffer);
+    }
+
+    @Post('import')
+    @UseInterceptors(FileInterceptor('file'))
+  async importFromExcel(@UploadedFile() file) {
+    await this.databaseService.importFromExcel(file.buffer);
+
+  }
+}
